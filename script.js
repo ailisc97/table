5e12b6d9c6ba41949ff639e032500939878d7f3a
ailisc97 <ailiscurran97@gmail.com>
2021-05-18 12:59:09 +0000

    show/hide

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $("#stream1_btn").on("click", function() {
-        $(".stream1").removeClass('highlight_stream');
-        $(".stream2").removeClass('highlight_stream');
-        $(".stream3").removeClass('highlight_stream');
-        $(".stream1").addClass('highlight_stream');
+        $(".stream1").hide();
+        $(".stream2").show('slow');
+        $(".stream3").hide('fast');
+        $(".stream1").show(1000);
    });
    $("#stream2_btn").on("click", function() {
         $(".stream1").removeClass('highlight_stream');
